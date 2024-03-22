2f3c387a5113e13f895aceade8a41ba1acfe790c
Parece <90103455+MiguelParece@users.noreply.github.com>
2024-03-22 15:56:33 +0000

    feat: assessment e2e test, checked for 6 instances in activity table
    issue #80

---
+++ frontend/tests/e2e/specs/assessment/assessment.js
@@ -0,0 +1,39 @@
+describe('Assessment', () => {
+    beforeEach(() => {
+        cy.deleteAllButArs();
+        cy.createEntitiesForAssessmentTesting();
+    });
+
+    afterEach(() => {
+        cy.deleteAllButArs();
+    });
+
+    it('assessActivityAndCheck', () => {
+        const REVIEW = 'Assessment demo review';
+        // ------------ VOLUNTEER TEST ------------
+        cy.demoVolunteerLogin();
+        // go to activities view
+        // intercepts necessary POST and GET requests
+        cy.intercept('POST', '/institutions/*/assessments', (req) => {
+            req.body = {
+                review: REVIEW
+            };
+        }).as('createAssessment');
+        cy.intercept('GET', '/users/getParticipations').as('getParticipations');
+        cy.intercept('GET', '/activities').as('getActivities');
+        cy.intercept('GET', '/users/getAssessments').as('getAssessments');
+        // go to volunteer activities view
+        cy.get('[data-cy="volunteerActivities"]').click();
+        // waits for the GET requests
+        cy.wait('@getParticipations');
+        cy.wait('@getActivities');
+        cy.wait('@getAssessments');
+        // verify that the activity table has 6 instances
+        cy.get('[data-cy="volunteerActivitiesTable"] tbody tr')
+            .should('have.length', 6);
+
+        cy.logout();
+
+
+    });
+});
